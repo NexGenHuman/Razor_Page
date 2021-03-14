d2590f5d716ddfaadc0a8cc692846ea7c86678ab
Konrad Wasilewski <konradwasilewski@vp.pl>
2021-03-14 22:49:06 +0100

    Done

---
+++ RazorPage/RazorPage/Models/Address.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace RazorPage.Models
+{
+    public class Address
+    {
+        [Display(Name = "Twoja ulubiona ulica")]
+        [StringLength(60, MinimumLength = 3), Required(ErrorMessage ="Pole jest wymagane")]
+        public string Street { get; set; }
+        [Display(Name = "Twój kod pocztowy")]
+        [DataType(DataType.PostalCode), Required(ErrorMessage = "Pole jest wymagane")]
+        public string ZipCode { get; set; }
+        [Display(Name = "Twoje miasto")]
+        [StringLength(60, MinimumLength = 3), Required(ErrorMessage = "Pole jest wymagane")]
+        public string City { get; set; }
+        [Display(Name = "Twój numer")]
+        [DataType(DataType.PhoneNumber), Required(ErrorMessage = "Pole jest wymagane")]
+        public int Number { get; set; }
+    }
+}
---
+++ RazorPage/RazorPage/Pages/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPage.Models;
+
+namespace RazorPage.Pages
+{
+    public class IndexModel : PageModel
+    {
+        [BindProperty]
+        public Address Address { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string Name { get; set; }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+                return Page();
+            return RedirectToPage("./Privacy");
+        }
+
+        public void OnGet()
+        {
+            if (string.IsNullOrWhiteSpace(Name))
+                Name = "User";
+        }
+    }
+}
